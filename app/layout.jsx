c39b61051c0e4f07451e07d5c648093c2d530e59
shafmaul23 <shafmaul23@gmail.com>
2024-08-25 11:03:19 +0700

    set up authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,28 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-  title: 'NyontekPrompt',
-  description: 'Temukan dan Bagikan Prompt AI'
-}
+  title: "NyontekPrompt",
+  description: "Temukan dan Bagikan Prompt AI",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className='app'>
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
-  )
+  );
 }
